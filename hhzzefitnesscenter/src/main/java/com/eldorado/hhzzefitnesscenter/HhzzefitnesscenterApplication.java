a2421c0400960eadc3be0539a7ac9068ab91efa5
Gabriel Poersch <gabpoersch@gmail.com>
2023-01-20 04:53:52 -0300

    Spring Initializr

---
+++ hhzzefitnesscenter/src/main/java/com/eldorado/hhzzefitnesscenter/HhzzefitnesscenterApplication.java
@@ -0,0 +1,13 @@
+package com.eldorado.hhzzefitnesscenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhzzefitnesscenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhzzefitnesscenterApplication.class, args);
+	}
+
+}
---
+++ hhzzefitnesscenter/src/test/java/com/eldorado/hhzzefitnesscenter/HhzzefitnesscenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eldorado.hhzzefitnesscenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhzzefitnesscenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
