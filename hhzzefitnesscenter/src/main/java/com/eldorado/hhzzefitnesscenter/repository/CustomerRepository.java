786f1cfb6cdc2ee830d3b494851d330dd513993a
Gabriel Poersch <gabpoersch@gmail.com>
2023-01-20 06:44:53 -0300

    forgot to create a branch

--- hhzzefitnesscenter/src/main/java/com/eldorado/hhzzefitnesscenter/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.eldorado.hhzzefitnesscenter.repository;
-
-import com.eldorado.hhzzefitnesscenter.model.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CustomerRepository extends MongoRepository<Customer, String> {
-
-}
